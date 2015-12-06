3cf48c6f1573cdb7c9feab0bb028e84e34f18f7b
Kevin Van Beek <kvnvanbeek@gmail.com>
2015-12-06 11:23:53 -0800

    new api

--- services/news.js
+++ services/news.js
@@ -6,7 +6,7 @@ angular
 
       getNews: function (stock){
         console.log(stock);
-        var url = "https://access.alchemyapi.com/calls/data/GetNews?apikey=aef49ecc535bf36b92a71a1b6d90ad98ad3247ed&return=enriched.url.title,enriched.url.url&start=1448755200&end=1449442800&q.enriched.url.enrichedTitle.entities.entity=|text=" + stock + ",type=company|&count=25&outputMode=json"
+        var url = "https://access.alchemyapi.com/calls/data/GetNews?apikey=6a9dbe15c569c89c02fb7beda136b3470afc1796&return=enriched.url.title,enriched.url.url&start=1448755200&end=1449442800&q.enriched.url.enrichedTitle.entities.entity=|text=" + stock + ",type=company|&count=25&outputMode=json"
         var promise = $http.get(url);
         console.log("running get news");
         return promise.then(function (response){
